29bbf039f5d9c9ab1d5e0e3b1181e360220fea5c
chen zefan <czfshine@outlook.com>
2016-10-20 16:45:32 +0800

    Update uoj1104.c

--- src/uoj1104.c
+++ src/uoj1104.c
@@ -1,92 +1,84 @@
-#include "stdio.h" 
-#include "malloc.h" 
-#define LEN sizeof(struct student) 
-//TODO
-struct student 
-{ 
-     long num; 
-     int score; 
-     struct student *next; 
-}; 
+#include "stdio.h"
+#include "malloc.h"
+#define LEN sizeof(struct student)
 
-struct student *create(int n) 
-{  
-     struct student *head=NULL,*p1=NULL,*p2=NULL; 
-     int i; 
-     for(i=1;i<=n;i++) 
-     {  p1=(struct student *)malloc(LEN); 
-        scanf("%ld",&p1->num); 
-        scanf("%d",&p1->score); 
-        p1->next=NULL; 
-        if(i==1) head=p1; 
-        else p2->next=p1; 
-        p2=p1; 
-      } 
-      return(head); 
-} 
+struct student
+{
+     long num;
+     int score;
+     struct student *next;
+};
 
-void print(struct student *head) 
-{ 
-    struct student *p; 
-    p=head; 
-    while(p!=NULL) 
-    { 
-        printf("%8ld%8d",p->num,p->score); 
-        p=p->next; 
-        printf("\n"); 
-    } 
-} 
+struct student *create(int n)
+{
+     struct student *head=NULL,*p1=NULL,*p2=NULL;
+     int i;
+     for(i=1;i<=n;i++)
+     {  p1=(struct student *)malloc(LEN);
+        scanf("%ld",&p1->num);
+        scanf("%d",&p1->score);
+        p1->next=NULL;
+        if(i==1) head=p1;
+        else p2->next=p1;
+        p2=p1;
+      }
+      return(head);
+}
 
-struct student *reverse(struct student *head) 
-{ 
-	struct student * pre=NULL ,*p=NULL,*temp=NULL,*res=NULL;
+void print(struct student *head)
+{
+    struct student *p;
+    p=head;
+    while(p!=NULL)
+    {
+        printf("%8ld%8d",p->num,p->score);
+        p=p->next;
+        printf("\n");
+    }
+}
 
-	p=head;
+struct student *reverse(struct student *head)
+{
+    struct student * end=NULL,*p,*q,*c;
 
-	while(p!=NULL){
-		if(p->next->next==NULL){
-			res=p->next;
-			p->next=NULL;
-			p=NULL;
-		}else{
-		p=p->next;}
-	}
-	temp=res;
-	p=head;
-	while(p!=NULL){
-		if(p->next->next==NULL){
-			res->next=p->next;
-			res=res->next;
-			p->next=NULL;
-			if(head->next==NULL){
-				res->next=head;
-				temp->next=res;
-				return temp;
-			}
-			if(head->next->next==NULL){
-				res->next=head->next;
-				res=res->next;
-			
-				res->next=head;
-				p=	NULL;
-				temp->next=res;
-				return temp;
-			}else{
-			p=head;}
-		}else{p=p->next;}
-	}
+    p=head;
 
-	return res;
+    while(1){
+        while(1){
+            if(p->next->next==NULL){
+                q=p->next;
+                p->next=NULL;
+                break;
+            }
 
-} 
+            p=p->next;
+        }
 
-main() 
-{ 
-    struct student *head,*stu; 
-    int n; 
-    scanf("%d",&n);   
-    head=create(n); 
-    print(head); 
-    head=reverse(head); 
-    print(head); 
-} 
+        if(end==NULL){
+        end=q;
+        c=q;
+        }else{
+        c->next=q;
+        c=q;
+        }
+
+        if (p==head){
+            c->next=p;
+            break;}
+
+        p=head;
+    }
+
+    return end;
+}
+
+main()
+{
+    struct student *head,*stu;
+    int n;
+    scanf("%d",&n);
+    head=create(n);
+    print(head);
+    head=reverse(head);
+    print(head);
+}
