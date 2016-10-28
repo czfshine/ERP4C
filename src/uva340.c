8af927a0ae7cd9e30bc4a456b40fa66d11100eaf
chen zefan <czfshine@outlook.com>
2016-10-28 22:24:32 +0800

    update uva340

--- src/uva340.c
+++ src/uva340.c
@@ -4,27 +4,25 @@ int main(int argc, char const *argv[])
 	int n;
 	while(scanf("%d",&n)==1&&n){
 		int data[n];
+		int c1[11]={0};
 		for (int i = 0; i < n; ++i)
 		{
 			scanf("%d",data+i);
+			c1[data[i]]++;
 		}
-
-		
 		int m=1;
 		while(m){
+			int c2[11]={0};
 			int A=0,B=0;
 			for (int i = 0; i < n; ++i)
 			{
 				scanf("%d",&m);
 				if(m==0) continue;
+				c2[data[m]]++;
 				if(data[i]==m) A++;
-
-				for (int j = 0; j < n; ++j)
-				{
-					if(data[j]==m) {B++;
-					printf("B %d %d %d\n",data[j],m,B);
-					break;}
-				}
+			}
+			for (int j = 1; j <= 9; ++j){
+				if(c1[j] < c2[j]) B += c1[j]; else B += c2[j];
 			}
 			if(m)
 				printf("%d %d\n",A,B);
