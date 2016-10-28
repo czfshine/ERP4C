15f1deac3be04949ab95c93ccadfadcd1152fa79
chen zefan <czfshine@outlook.com>
2016-10-28 22:47:21 +0800

    update uoj1035

--- src/uoj1035.c
+++ src/uoj1035.c
@@ -1,40 +1,24 @@
 #include "stdio.h"	
-
 int main(int argc, char const *argv[])
 {
-	
 	int n;
 	scanf("%d",&n);
 	n++;
 	for (int i = 0; i < n; ++i)
 	{
 		for (int j = 1; j < n-i; ++j)
-		{
 			printf(" ");
-		}
 		for (int k = 0; k < i*2+1 ; ++k)
-		{
 			printf("*");
-		}
-		
 		printf("\n");
-
 	}
-
 	for (int i = 0; i < n-1; ++i)
 	{
 		for (int j = 0; j < i+1; ++j)
-		{
 			printf(" ");
-		}
 		for (int k = 3; k < (n-i)*2 ; ++k)
-		{
 			printf("*");
-		}
-		
 		printf("\n");
-
 	}
-
 	return 0;
 }
