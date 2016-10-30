950d596ce98f73d66c75b723feda4dbab873c9de
chen zefan <czfshine@outlook.com>
2016-10-30 13:09:07 +0800

    update uoj1225

--- src/uoj1225.c
+++ src/uoj1225.c
@@ -1,23 +1,26 @@
 #include "stdio.h"
 int main(int argc, char const *argv[])
-{
-	int n;
-	scanf("%d",&n);
-	int count[10]={0};
-	for (int i = 1; i <= n; ++i)
-	{
-		int t=i;
-		while(t){
-			//printf("-%d\n",t%10); 
-			count[t%10]++;
-			t/=10;
-			//printf("+%d",t);
-		}
+{	int T;
+	scanf("%d",&T);
+	while(T--){
+		int n;
+		scanf("%d",&n);
+		int count[10]={0};
+		for (int i = 1; i <= n; ++i)
+		{
+			int t=i;
+			while(t){
+				//printf("-%d\n",t%10); 
+				count[t%10]++;
+				t/=10;
+				//printf("+%d",t);
+			}
 
-	}
-	for (int i = 0; i < 10; ++i)
-	{
-		printf("%d\n",count[i]);
+		}
+		for (int i = 0; i < 10; ++i)
+		{
+			printf("%d ",count[i]);
+		}
 	}
 	return 0;
 }
