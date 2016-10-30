9072db991821064180cb5bbe7fb53566630a88f7
chen zefan <czfshine@outlook.com>
2016-10-30 17:32:44 +0800

    update uva1339

---
+++ src/uva1339.c
@@ -0,0 +1,47 @@
+#include "stdio.h"
+int main(int argc, char const *argv[])
+{
+	int c1[26],c2[26];
+	char s1,s2;
+	INT TEMP;
+	while(s1=gets()){
+		s2=gets();
+
+		for (int i = 0; i < strlen(s1); ++i)
+			c1[s1[i]-'A']++;
+		for (int i = 0; i < strlen(s2); ++i)
+			c2[s2[i]-'A']++;
+
+		for (int i = 0; i < 26; ++i)
+		{
+			for (int j = 0; j < i; ++j)
+			{
+				if(c1[i]>c1[j]){
+					temp=c1[i];c1[i]=c1[j];c1[j]=temp;
+				}
+			}
+		}
+		for (int i = 0; i < 26; ++i)
+		{
+			for (int j = 0; j < i; ++j)
+			{
+				if(c2[i]>c2[j]){
+					temp=c2[i];c2[i]=c2[j];c2[j]=temp;
+				}
+			}
+		}
+		for (int i = 0; i < 26; ++i)
+		{
+			if(c1[i]!=c2[i])
+				break;
+		}
+		if(i==26){
+			printf("YES");
+		}else{
+			printf("NO")
+		}
+
+	}
+
+	return 0;
+}
