8f091a93da52776dada0e91579fe865036236589
chen zefan <czfshine@outlook.com>
2016-10-30 17:44:25 +0800

    update uva1339

--- src/uva1339.c
+++ src/uva1339.c
@@ -35,9 +35,9 @@ int main(int argc, char const *argv[])
 		int i;
 		for ( i= 0; i < 26; ++i)
 		{	
-			printf("%d %d",c1[i],c2[i]);
-			//if(c1[i]!=c2[i])
-				//break;
+			//printf("%d %d",c1[i],c2[i]);
+			if(c1[i]!=c2[i])
+				break;
 		}
 		if(i==26){
 			printf("YES\n");
