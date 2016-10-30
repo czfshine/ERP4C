2da433561bbaacf9af6321756d054f4540d80d9b
chen zefan <czfshine@outlook.com>
2016-10-30 13:03:36 +0800

    update uoj1225

---
+++ src/uoj1225.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+int main(int argc, char const *argv[])
+{
+	int n;
+	scanf("%d",n);
+	int count[10];
+	for (int i = 1; i < n; ++i)
+	{
+		int t=i;
+		while(t){ 
+			count[t%10]++;
+			t/=10;
+		}
+
+	}
+	for (int i = 0; i < 10; ++i)
+	{
+		printf("%d\n",count[i]);
+	}
+	return 0;
+}
