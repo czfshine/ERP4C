9a6ac48bee707c0459b46e9781c0f6a7d4b2a0ac
czfshine <czfshine@outlook.com>
2016-11-01 14:42:00 +0800

    update uoj_test68-2

---
+++ src/uoj_test68-1.c
@@ -0,0 +1,8 @@
+#include "stdio.h"
+int main(int argc, char const *argv[])
+{
+	char ch;
+	scanf("%c",&ch);
+	printf("%d",ch);
+	return 0;
+}
---
+++ src/uoj_test68-2.c
@@ -0,0 +1,9 @@
+#include "stdio.h"
+#define PI 3.14159
+int main(int argc, char const *argv[])
+{
+	double r,h;
+	scanf("%lf%lf",r,h);
+	printf("%.3lf",PI*r*r*2+PI*r*2*h);
+	return 0;
+}
