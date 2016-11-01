1289629a1600a3866443e66d41dafedc231f4864
czfshine <czfshine@outlook.com>
2016-11-01 14:48:48 +0800

    update uoj_test70-1

---
+++ src/uoj_test70-1.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+#include "math.h"
+int main(int argc, char const *argv[])
+{
+	int n,count=0;
+	scanf("%d",&n);
+	for (int i = 1; i <=n; ++i)
+	{
+		if(n%i==0) {
+			count++;
+			//n/=i;
+		}
+	}
+	printf("%d",count)
+	return 0;
+}
