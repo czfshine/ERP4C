a51df752bfb7213bfe688ace12a6e27b345cb2ac
czfshine <czfshine@outlook.com>
2016-11-01 15:08:28 +0800

    update uoj_test73-1

---
+++ src/uoj_test73-1.c
@@ -0,0 +1,32 @@
+#include "stdio.h"
+int is(char * str,int len){
+	int len,fail=0;
+	if((len%2)==0){
+		for (int i = 0; i < len/2; ++i)
+		{
+			if(str[i]==str[len-i-1]){}else{fail=1;}
+		}
+	}else{
+		for (int i = 0; i < (len-1)/2; ++i)
+		{
+			if(str[i]==str[len-i-1]){}else{fail=1;}
+		}
+	}
+
+	if(fail){
+		return 0;
+	}else{
+		return 1;
+	}
+}
+int main(int argc, char const *argv[])
+{
+	char str[81];
+	for(int i=0;(str[i]=getchar())!='\n';i++);
+	if(is(str,i)){
+		printf("Y");
+	}else{
+		printf("N");
+	}
+	return 0;
+}
