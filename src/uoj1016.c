68092598eee636aa855bcefa74e2abd98c5675f9
czfshine <czfshine@outlook.com>
2016-11-03 08:12:35 +0800

    update uoj1016

--- src/uoj1016.c
+++ src/uoj1016.c
@@ -1,13 +1,11 @@
 #include "stdio.h"
 main(){
     char str[6];
-
     scanf("%s",str);
     int i;
-    for(i=0;i<6;i++){
-        if(str[i]<123&&str[i]>64){
-        str[i]=str[i]<91?str[i]+32:str[i];}
-    }
+    for(i=0;i<6;i++)
+        if(str[i]<123&&str[i]>64)
+        	str[i]=str[i]<91?str[i]+32:str[i];
     str[5]='\0';
     printf("%s",str);
 }
