36313593ed11314a9f55ed8dd2b4dffe091091d6
czfshine <czfshine@outlook.com>
2016-11-03 08:13:30 +0800

    update uoj1018

--- src/uoj1018.c
+++ src/uoj1018.c
@@ -3,16 +3,12 @@ main(){
 	int data[3];
     int i,j;
 	scanf("%d,%d,%d",&data[0],&data[1],&data[2]);
-	for( i=0;i<3;i++){
-        for ( j=0;j<i;j++){
+	for( i=0;i<3;i++)
+        for ( j=0;j<i;j++)
             if(data[i]>data[j]){
                 int temp=data[i];
                 data[i]=data[j];
                 data[j]=temp;
             }
-        }
-
-    }
-
     printf("%d,%d,%d",data[2],data[1],data[0]);
 }
