ebf210cc78725a016d92156fbc68a96ab43fd7b0
czfshine <czfshine@outlook.com>
2016-11-03 08:14:41 +0800

    update uoj1019

--- src/uoj1019.c
+++ src/uoj1019.c
@@ -3,18 +3,10 @@
 int main(int argc, char const *argv[])
 {
 	int nums[5]={0};
-
 	int i=0;
-
 	for (int i = 0; i < 5; ++i)
-	{
 		scanf("%d",&nums[i]);
-	}
-
 	for (int i = 0; i < 5; ++i)
-	{
 		printf("%s",nums[i]%27?"NO\n":"YES\n");
-	}
-
 	return 0;
 }
