eac9fde500266882cd53eb99923e6d6af9ec326b
czfshine <czfshine@outlook.com>
2016-11-03 23:03:34 +0800

    update uoj17086

--- src/uoj17086.c
+++ src/uoj17086.c
@@ -1,17 +1,18 @@
 #include "stdio.h"
 //todo
 void p(char * str,int len){
-	printf("%c",str[len-1]);
+	
 	for (int i =0 ; i<len; ++i)
 	{
 		p(str,len-1-i);
 		
 	}
+	printf("%c",str[len-1]);
 }
 int main(int argc, char const *argv[])
 {
 	
-	char str[10];
+	char str[10]={0};
 	int n;
 	scanf("%d",&n);
 	for (int i = 0; i < n-1; ++i)
