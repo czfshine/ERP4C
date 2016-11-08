e15a9cb3dd9eb89d91a7e36c3dd9f1358d06db31
czfshine <czfshine@outlook.com>
2016-11-08 15:17:31 +0800

    update uoj10302

--- src/uoj10302.c
+++ src/uoj10302.c
@@ -1,8 +1,25 @@
 #include "stdio.h"
+#include "math.h"
+
 int data[10000][10000];
+
+long f(int n,int k){
+
+	if(k==0) return 1;
+
+	if(k>=floor(log(n))) return f(n,floor(log(n)));
+	int long sum=0;
+	if(k>0) {
+		for (int i = 0; i <=k; ++i)
+		{
+			sum+=f(n-2^i, i);
+		}
+		return sum;
+	}
+}
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	printf("%ld",f(5,0));
 	return 0;
 }
