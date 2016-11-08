c9ef363eeccc904be2f293d227ccb467a10b1208
czfshine <czfshine@outlook.com>
2016-11-08 15:19:03 +0800

    update uoj10302

--- src/uoj10302.c
+++ src/uoj10302.c
@@ -9,13 +9,11 @@ long f(int n,int k){
 
 	if(k>=floor(log(n))) return f(n,floor(log(n)));
 	int long sum=0;
-	if(k>0) {
 		for (int i = 0; i <=k; ++i)
 		{
 			sum+=f(n-pow(2,i), i);
 		}
 		return sum;
-	}
 }
 
 int main(int argc, char const *argv[])
