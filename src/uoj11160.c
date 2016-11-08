8bd49146c8b20888d43f356a410eff9a4f9853a6
czfshine <czfshine@outlook.com>
2016-11-08 14:53:29 +0800

    update uoj11160

---
+++ src/uoj11160.c
@@ -0,0 +1,27 @@
+#include "stdio.h"
+
+#define max(x,y) (x)>(y)?x:y
+#define abs(x) x>0?x:-x
+int main(int argc, char const *argv[])
+{
+	char ch;
+	int c=0,m=0;
+
+	while(scanf("%c",&ch)>0){
+		if(ch=='\n'){
+
+			printf("%d",m);
+			c=0,m=0;
+			continue;
+		}
+		if(ch=='+'){
+			c+=1;
+		}else{
+			c-=1;
+		}
+		m=max(m,abs(c));
+
+	}
+	printf("%d",m);
+	return 0;
+}
