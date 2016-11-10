e0409b5d730afacea47f73905e94adbefca95381
czfshine <czfshine@outlook.com>
2016-11-10 16:45:55 +0800

    update uoj18063

--- src/uoj18063.c
+++ src/uoj18063.c
@@ -1,10 +1,11 @@
 #include "stdio.h"
 //todo
+long long data[1000000];
 int main(int argc, char const *argv[])
 {
 	long long n,m=3;
 	scanf("%lld",&n);
-	long long data[n+1];
+	
 	data[1]=1;
 	for (long long i = 2; i <= n; ++i)
 		data[i] = (data[i-1]+m-1)%i + 1;
