7de6c0e6e88fa784d6a218c72eb341e697e00de6
czfshine <czfshine@outlook.com>
2016-11-10 16:42:51 +0800

    update uoj18063

--- src/uoj18063.c
+++ src/uoj18063.c
@@ -4,7 +4,8 @@ int main(int argc, char const *argv[])
 {
 	int n,m=3;
 	scanf("%d",&n);
-	int data[n+1]={1};
+	int data[n+1];
+	data[1]=1;
 	for (int i = 2; i <= n; ++i)
 		data[i] = (data[i-1]+m-1)%i + 1;
 	printf("%d",data[n]);
