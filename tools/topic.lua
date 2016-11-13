9ee37956a8c5cae17c5dc96f080f2d7d0b682932
czfshine <czfshine@outlook.com>
2016-11-13 11:10:54 +0800

    update script

--- tools/topic.lua
+++ tools/topic.lua
@@ -1,1 +1,20 @@
+package.path=package.path..";./luagy/?.lua"
 
+attr=require "file/attrdir"
+match=require "string/match"
+local has=match.has
+
+attr("../src",function (filename,path)
+
+
+	print(path)
+	--print(path:gsub(".+/(.+)%.c",print))
+
+	if has(".+/(.+)%.c",path,function(s) out=s end) then
+		print(out)
+	else
+		--print("This world is mad !")
+	end
+
+end
+)
