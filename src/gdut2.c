2643e3aef5b2f76f70c2f15ba4fe58c71111a55a
czfshine <czfshine@outlook.com>
2016-11-13 15:10:45 +0800

    update gdut2

---
+++ src/gdut2.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+
+int main(int argc, char const *argv[])
+{
+	int T;
+	scanf("%d",&T);
+
+	for (int i = 0; i < T; ++i)
+	{
+		int x,y,n1,r,n,m;
+		scanf("%d%d%d",&x,&y,&n1);
+		x++;y++;
+		n=x,m=y;
+		x>y?r=x,x=y,y=r:1;
+    	while ((r=y%x)!=0) 
+   		{ 
+        	y=x; 
+       	 	x=r; 
+    	}
+
+    	printf("%d\n",n1/(m*n/x)*6);
+	}
+	return 0;
+}
