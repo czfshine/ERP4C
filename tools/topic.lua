472625c22fca03cb2feb846387055a31e43595d8
czfshine <czfshine@outlook.com>
2016-11-13 13:06:22 +0800

    about html con

--- tools/topic.lua
+++ tools/topic.lua
@@ -4,6 +4,8 @@ attr=require "file/attrdir"
 match=require "string/match"
 local has=match.has
 
+net=require "network/connector"
+
 local list={}
 attr("../src",function (filename,path)
 
@@ -21,10 +23,20 @@ attr("../src",function (filename,path)
 
 end
 )
+require("socket")
+function sleep(n)
+   socket.select(nil, nil, n)
+end
+
 
 table.foreach(list,function (_,v)
-
-
-
+	infile=io.open("../data/uoj"..v..".html","w")
+	r,c,cookie=net.http.get("http://172.26.14.60:8000/uoj/common_problem_view_PUBLIC.html?identity=f139b91c0058db9039f8090cf6d56e4a&problemId="..v,
+		{cookie="JSESSIONID=99ECD3D3646A3F74EFD3637EFCC7689B; cotree=1; cstree=14; OUTFOX_SEARCH_USER_ID_NCOO=1992381091.4441192"} )
+	infile:write(r)
+	print(v)
+	infile:close()
+	print(c)
+	sleep(15)
 end
 )
