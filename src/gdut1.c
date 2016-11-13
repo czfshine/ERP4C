33b26158f8a55dcb0b376f5f72f9286e672a6ff4
czfshine <czfshine@outlook.com>
2016-11-13 14:47:34 +0800

    update gdut1

--- src/gdut1.c
+++ src/gdut1.c
@@ -7,27 +7,29 @@ int main(int argc, char const *argv[])
 
 	for (int i = 0; i < T; ++i)
 	{
-		int n,k,l,t,m;
+		int n,k,l,t=0,m;
 		scanf("%d",&n);
 		int a[n];
 
-		for (int j = 0; j < n; ++j)
+		for (int j = 0; j < 1
+5
+156 22 223 156 18n; ++j)
 		{
 			scanf("%d",a+j);
 		}
 
-		for(k=0;k<10;k++) 
+		for(k=0;k<n;k++) 
    		{
    			for(l=0;l<k;l++) 
         		 if (a[k]<a[l]) 
             		{t=a[k];a[k]=a[l];a[l]=t;} 
    		} 
 
-   		for (m = 0; m <=(n + 1)/ 2; ++m)
+   		for (m = 0; m <(n + 1)/ 2; ++m)
    		{
    			 printf("%d ",a[m]);
    		}
-   		
+
    		for (m = (n + 1) / 2; m <n; ++m)
    		{
    			 printf("%d ",a[m]);
