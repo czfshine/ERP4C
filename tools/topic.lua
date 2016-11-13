652712780176dc9f22dbef1dc3dbb43753535e12
czfshine <czfshine@outlook.com>
2016-11-13 11:27:55 +0800

    about uoj list

--- tools/topic.lua
+++ tools/topic.lua
@@ -4,17 +4,27 @@ attr=require "file/attrdir"
 match=require "string/match"
 local has=match.has
 
+local list={}
 attr("../src",function (filename,path)
 
 
-	print(path)
+	--print(path)
 	--print(path:gsub(".+/(.+)%.c",print))
 
 	if has(".+/(.+)%.c",path,function(s) out=s end) then
-		print(out)
-	else
-		--print("This world is mad !")
+		--print(out)
+
+		out:gsub("uoj(%d+)",function (n)
+			table.insert(list,n)
+		end )
 	end
+
+end
+)
+
+table.foreach(list,function (_,v)
+
+
 
 end
 )
