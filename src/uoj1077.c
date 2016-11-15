828a30b1eeb9a2b7817afb2aefbfefce46c3bc18
czfshine <czfshine@outlook.com>
2016-11-15 14:58:17 +0800

    update uoj1077

---
+++ src/uoj1077.c
@@ -0,0 +1,31 @@
+#include "stdio.h"
+int main(int argc, char const *argv[])
+{
+	int d[8],b[8];
+
+	for (int i = 0; i < 8; ++i)
+	{
+		scanf("%d",d+i);
+	}
+
+	for (int i = 0; i < 8; ++i)
+	{
+		scanf("%d",b+i);
+	}
+
+
+	int x,y;
+
+	x=d[0];
+	y=b[0];
+
+	for (int j = ; j < 8; ++j)
+	{
+		y-=b[j];
+		if(y<0) y=b[j]+y;
+		x*=d[j];
+		y*=d[j];
+	}
+	printf("%d",x+y);
+	return 0;
+}
