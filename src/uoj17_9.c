cbeb13fff0e77a1e8c6bde4ce6ea57fdd18195bd
czfshine <czfshine@outlook.com>
2016-11-17 17:55:32 +0800

    update uoj17_9

--- src/uoj17_9.c
+++ src/uoj17_9.c
@@ -1,11 +1,31 @@
 #include "stdio.h"
 #include "string.h"
 #include "stdlib.h"
-char * d;
 
+int f(long n){
+	long k=1;
+	int c=0;
+	while(k<n){
+		k*=5;
+		c++;
+	}
+	int j=1;
+	long sum=0;
+	int n1=4;
+	for (int i = 0; i < c; ++i)
+	{	
 
+		for (int i = 0; i < n1; ++i)
+		{
+			sum+=j
+		}
+		j++;
+		n1*=5;
+	}
+	printf("%d",sum);
+	return c;
+}
 int main(int argc, char const *argv[])
-{
-	d=(char *)calloc(1000000000,1);
+{	
 	return 0;
 }
