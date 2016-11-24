86bb7c19a34572be499aa0526f78d77f3bdcbdbe
czfshine <czfshine@outlook.com>
2016-11-24 17:29:41 +0800

    update uoj24_10

---
+++ src/uoj24_10.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+int main(int argc, char const *argv[])
+{
+	int T;
+	scanf("%d",&T);
+
+	while(T--){
+		int n;
+		scanf("%d",&n)
+		int m=n/5;
+		int h;
+		for (int i = 1; i =< m; ++i)
+		{	int count=0;
+			while(i%5==0){
+				count++;
+			}
+			printf("%d",count);
+		}
+	}
+	return 0;
+}
