b095dad9293ba290638b8cf35f4bdb567c53f45b
czfshine <czfshine@outlook.com>
2016-11-24 17:43:38 +0800

    update uoj24_2

--- src/uoj24_2.c
+++ src/uoj24_2.c
@@ -1,3 +1,13 @@
+/*
+描述
+由键盘输入一个句子（字符个数不定，最多不超过80个，以'\n'结束），将其中的大写字符变成小写（其它类型的字符不变），
+最后输出变换后的句子。
+
+输入样例
+ThiS IS My fIrSt C ProgrAm!
+输出样例
+this is my first c program!
+*/
 #include "stdio.h"
 
 int main(int argc, char const *argv[])
