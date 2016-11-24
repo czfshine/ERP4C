e6bd661062b4821b5a32e796fdb2881c128f2891
czfshine <czfshine@outlook.com>
2016-11-24 17:22:52 +0800

    update uoj24_8

--- src/uoj24_8.c
+++ src/uoj24_8.c
@@ -48,10 +48,10 @@ int main(int argc, char const *argv[])
 		for (int j = 0; j < N; ++j)
 		{
 			int n=*(data+j*N+i);
-			int xd=x[i];
-			int yd=y[j];//fuck
+			int xd=x[j];
+			int yd=y[i];//fuck
 			int m=xd+yd-n;
-			printf("%d %d %d %d %d %d\n",i,j,xd,yd,n,m);
+			//printf("%d %d %d %d %d %d\n",i,j,xd,yd,n,m);
 			max=max>m?max:m;
 		}
 	}
