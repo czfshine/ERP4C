f9aaa59458da4bd2765e2cc1207b290420f152bb
czfshine <czfshine@outlook.com>
2016-11-24 16:46:31 +0800

    update uoj24_3

---
+++ src/uoj24_3.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+int F(int x, int n) 
+{ 
+    if(n==1) return x; 
+    else return x*F(x,n-1); 
+}
+
+int main() 
+{ 
+    int x, n; 
+    scanf("%d%d", &x, &n); 
+    printf("%d", F(x, n)); 
+    return 0; 
+} 
