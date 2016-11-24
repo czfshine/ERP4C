c4efb8dc1f715bb0f5d7083b33633ff1cf0f2334
czfshine <czfshine@outlook.com>
2016-11-24 17:02:05 +0800

    update uoj24_6

---
+++ src/uoj24_6.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "math.h"
+
+#define min(x,y) (x)>(y)?(x):(y)
+double dist(int x ,int y,int dx,int dy){
+
+	return sqrt(fabs((dx-x)*(dx-x)+(dy-y)*(dy-y)));
+}
+int main(int argc, char const *argv[])
+{
+	int data[50][2];
+	int T=;
+	scanf("%d",&T);
+	for (int i = 0; i < T; ++i)
+	{
+		scnaf("%d%d",&data[i][0],&data[i][1]);
+	}
+
+	double m=dist(data[0][0] ,data[0][1],data[1][0],data[1][1]);
+	for (int i = 0; i < T; ++i)
+	{
+		for (int j = 0; j < T; ++j)
+		{
+			if(i!=j)
+				m=min(dist(data[i][0] ,data[i][1],data[j][0],data[j][1]),m);
+		}
+	}
+		
+		printf("%lf",m);
+	return 0;
+}
