659f642916d5394374699eb5dc007d7e9ea8c698
czfshine <czfshine@outlook.com>
2016-11-24 16:42:51 +0800

    update uoj24_1

---
+++ src/uoj24_1.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+int main(int argc, char const *argv[])
+{
+	char ch;
+	for (int i = 0; i < 5; ++i)
+	{
+		scanf("%c",&ch);
+		if(ch>'A' &&ch<'Z' )
+			ch-=32;
+		printf("%c",ch);
+	}
+	return 0;
+}
