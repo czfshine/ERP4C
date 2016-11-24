4f39f9640eb19d52627ef2807731dd8b8bbc1bfc
czfshine <czfshine@outlook.com>
2016-11-24 16:53:08 +0800

    update uoj24_5

--- src/uoj24_5.c
+++ src/uoj24_5.c
@@ -1,24 +1,50 @@
-#include "stdio.h"	
+#include "stdio.h"
+#define abs(x) x>0?x:-x
 int main(int argc, char const *argv[])
 {
 	int n;
 	scanf("%d",&n);
-	n++;
+
+	if(n==1){
+		printf("*");
+		return 0;
+	}
+
 	for (int i = 0; i < n; ++i)
 	{
-		for (int j = 1; j < n-i; ++j)
-			printf(" ");
-		for (int k = 0; k < i*2+1 ; ++k)
-			printf("*");
-		printf("\n");
+		printf("*");
 	}
-	for (int i = 0; i < n-1; ++i)
+	printf("\n");
+	for (int i = n/2-n+2; i < n-n/2-1; ++i)
 	{
-		for (int j = 0; j < i+1; ++j)
-			printf(" ");
-		for (int k = 3; k < (n-i)*2 ; ++k)
+		//printf("%d",i);
+
+		int k=abs(i);
+
+		for (int i = 0; i < k+1; ++i)
+		{
 			printf("*");
+		}
+		for (int i = 0; i < (n-2)-(k*2); ++i)
+		{
+			printf(" ");
+		}
+		for (int i = 0; i < k+1; ++i)
+		{
+			printf("*");
+		}
+
 		printf("\n");
+
+
 	}
+
+
+	for (int i = 0; i < n; ++i)
+	{
+		printf("*");
+	}
+	printf("\n");
+
 	return 0;
 }
