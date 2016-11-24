f1fd8e440dd1dc099927a1f4ba1eb3f50407318d
czfshine <czfshine@outlook.com>
2016-11-24 17:02:55 +0800

    update uoj24_6

--- src/uoj24_6.c
+++ src/uoj24_6.c
@@ -17,12 +17,15 @@ int main(int argc, char const *argv[])
 	}
 
 	double m=dist(data[0][0] ,data[0][1],data[1][0],data[1][1]);
+	printf("%lf",m);
 	for (int i = 0; i < T; ++i)
 	{
 		for (int j = 0; j < T; ++j)
 		{
-			if(i!=j)
+			if(i!=j){
 				m=min(dist(data[i][0] ,data[i][1],data[j][0],data[j][1]),m);
+				printf("%lf",m);
+			}
 		}
 	}
 		
