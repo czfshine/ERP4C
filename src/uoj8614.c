1e223415c7683b116691eacb91c0fe7e750d70c9
czfshine <czfshine@outlook.com>
2016-12-07 23:12:26 +0800

    update uoj8614

---
+++ src/uoj8614.c
@@ -0,0 +1,35 @@
+#include <stdio.h>  
+#include <math.h>  
+int pc[100001];  
+int p [100001] 
+
+int count = 1;  
+
+int Prime(int n)  
+{  
+    int tmp = (int)sqrt((double)n);  
+    for (int i = 0; p[i] <= tmp; ++i) {  
+        if (n % p[i] == 0)  
+            return 0;  
+    }  
+    return 1;  
+}  
+int main()  
+{  
+    p[0] = 2;  
+    pc[2] = 1;  
+    for (int i = 3; i <= 100000; ++i) {  
+        if (Prime(i))  
+            p[count++] = i;
+        pc[i] = count;  
+    }  
+  
+    int T;  
+    scanf("%d", &T);  
+    while (T--) {  
+        int m;  
+        scanf("%d", &m);  
+        printf("%d\n", pc[m]);  
+    }  
+    return 0;  
+}  
