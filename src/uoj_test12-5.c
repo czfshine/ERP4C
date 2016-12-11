1820543b47aac25ff4febe822f4863e9af92be35
czfshine <czfshine@outlook.com>
2016-12-11 10:48:37 +0800

    update uoj_test12-5

--- src/uoj_test12-5.c
+++ src/uoj_test12-5.c
@@ -1,11 +1,14 @@
 #include "stdio.h"
 char lucky(int n){
 	int a;
+	char has4=0,has8=0;
 	while(n>0){
 		a=n%10;
 		n/=10;
-		printf("%d",a);
+		if(a==4)has4=1;
+		if(a==8)has8=1;
 	}
+	return (has8 && (!has4))
 }
 int main(int argc, char const *argv[])
 {
