32087fcd38fdf918244a2d38d8ae89d21e2ea827
czfshine <czfshine@outlook.com>
2016-12-11 11:07:10 +0800

    update uoj_test12-7

---
+++ src/uoj_test12-7.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+#include "math.h"
+#define min(x,y) (x)<(y)? (x):(y)
+int main(int argc, char const *argv[])
+{
+	float data[10];
+	for (int i = 0; i < 10; ++i)
+	{
+		scanf("%f",data+i);
+	}
+	float m=fabs(data[1]-data[0]);
+	for (int i = 1; i < 10; ++i)
+	{
+		for (int j = 0; j < i; ++j)
+		{
+			m=min(m,fabs(data[i]-data[j]));
+		}
+	}
+	printf("%f",m);
+	return 0;
+}
