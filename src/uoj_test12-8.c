4879241a7b68c2c41e2c09155c45a67da7ceeac3
czfshine <czfshine@outlook.com>
2016-12-11 11:10:22 +0800

    update uoj_test12-8

--- src/uoj_test12-8.c
+++ src/uoj_test12-8.c
@@ -5,7 +5,7 @@ main()
     FILE *fp; 
     char ch; 
 
-    if((fp=fopen("case.in","r"))==NULL) 
+    if((fp=fopen("case1.in","r"))==NULL) 
         return 0; 
     while((ch=getc(fp))!=EOF) 
     { 
