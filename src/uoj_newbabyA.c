648ca7de87fe8998ea2ba8e1425a13cdf700ff5c
czfshine <czfshine@outlook.com>
2016-12-11 23:34:56 +0800

    update uoj_newbabyA

--- src/uoj_newbabyA.c
+++ src/uoj_newbabyA.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 int main(int argc, char const *argv[])
 {
-	int a;
-	scanf("%d",&a);
-	a=a*a;
+	int n,flag=0;
+	scanf("%d",&n);
+	a=n*n;
 	for (int i =a%2?1:2; i < a; i+=2)
 	{
 
@@ -12,9 +12,14 @@ int main(int argc, char const *argv[])
 			int c=(i+a/i)/2;
 			int b=abs((i-a/i)/2);
 			if(b*b+a==c*c&& b!=0)
-				printf("%d %d\n",b,c);
+				if(n<b&&n<c){
+					printf("%d %d\n",b,c);
+					flag=1;
+				}
 		}
 
 	}
+
+	if(!flag) printf("N0");
 	return 0;
 }
