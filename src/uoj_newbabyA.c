4f6ee68de859775428cd31e97ae1cd2932b5a1dc
czfshine <czfshine@outlook.com>
2016-12-11 23:30:36 +0800

    update uoj_newbabyA

--- src/uoj_newbabyA.c
+++ src/uoj_newbabyA.c
@@ -5,8 +5,9 @@ int main(int argc, char const *argv[])
 	int a;
 	scanf("%d",&a);
 	a=a*a;
+	printf("1");
 	for (int i =a%2; i < a; i+=2)
-	{
+	{printf("1");
 		if(a%i==0){
 			int c=(i+a/i)/2;
 			int b=abs((i-a/i)/2);
@@ -15,6 +16,6 @@ int main(int argc, char const *argv[])
 		}
 
 	}
-	
+printf("1");
 	return 0;
 }
