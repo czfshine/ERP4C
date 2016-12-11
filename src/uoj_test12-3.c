d44fa173f698dbe9b9cd46e0f1136dcea5807a96
czfshine <czfshine@outlook.com>
2016-12-11 10:24:48 +0800

    update uoj_test12-3

--- src/uoj_test12-3.c
+++ src/uoj_test12-3.c
@@ -1,15 +1,19 @@
 #include "stdio.h"
-
-long long f(int n){
-	if(n==1)return 1;
-	return f(n-1)*n;
-}
-
 int main(int argc, char const *argv[])
 {
 	int n,m;
 	scanf("%d%d",&m,&n);
-	if(n>m){n=m-n;}
-	printf("%d",f(m)/f(m-n)/f(n));
+	int sum=1;
+	if(n<m/2){n=m-n;}
+	for (int i = m; i > n; --i)
+	{
+		sum*=i;
+	}
+	int sum2=1;
+	for (int i = 1; i <= n; ++i)
+	{
+		sum2*=i;
+	}
+	printf("%d",sum/sum2);
 	return 0;
 }
