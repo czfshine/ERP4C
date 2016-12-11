ae31d83fa30699e1849418f95a17cef66ef0a41d
czfshine <czfshine@outlook.com>
2016-12-11 10:58:09 +0800

    update uoj_test12-6

---
+++ src/uoj_test12-6.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+int main(int argc, char const *argv[])
+{
+	int n;
+	scanf("%d",&n);
+
+	int k=n/2;
+	for (int j = -k; j < k; ++j)
+	{
+		for (int i = 0; i < abs(j); ++i)
+		{
+			printf("*")；
+		}
+		for (int i = 0; i < abs(k+j); ++i)
+		{
+			printf(" ");
+
+		}
+
+		printf("0\n");
+	}
+	return 0;
+}
