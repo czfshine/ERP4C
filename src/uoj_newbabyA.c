7003b08e862e0e8e86e59b89ddafef27b6eb1d07
czfshine <czfshine@outlook.com>
2016-12-11 23:22:32 +0800

    update uoj_newbabyA

---
+++ src/uoj_newbabyA.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+int main(int argc, char const *argv[])
+{
+	int n;
+	scanf("%d",&n);
+	for (int i =a%2; i < a; i+=2)
+	{
+		if(a%i==0){
+			printf("%d\n",a%i);
+		}
+	}
+	return 0;
+}
