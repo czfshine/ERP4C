abd447f54e00cc78e839c19d8d59361f2b7a6e20
czfshine <czfshine@outlook.com>
2016-12-12 00:24:06 +0800

    update uoj_newbabyA

--- src/uoj_newbabyA.c
+++ src/uoj_newbabyA.c
@@ -3,19 +3,19 @@
 #define min(x,y) x<y?x:y
 int main(int argc, char const *argv[])
 {
-	int a,n,flag=0;
-	scanf("%d",&n);
+	long long  a,n,flag=0;
+	scanf("%lld",&n);
 	a=n*n;
-	for (int i =a%2?1:2; i < a; i+=2)
+	for (long long  i =a%2?1:2; i < a; i+=2)
 	{
 
 		if((a%i)==0){
 			//printf("%d %d\n",a,i);
-			int c=(i+a/i)/2;
-			int b=abs((i-a/i)/2);
+			long long c=(i+a/i)/2;
+			long long b=abs((i-a/i)/2);
 			if(b*b+a==c*c&& b!=0)
 				if((n<b&&n<c)&&(n+b>c&&n+c>b&&b+c>n)){
-					printf("%d %d",b,c);
+					printf("%lld %lld",b,c);
 					flag=1;
 					break;
 				}
