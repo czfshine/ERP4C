b6616a0dfc9b158b2c5ee3d2762a1c101a83429c
czfshine <czfshine@outlook.com>
2016-12-11 10:16:15 +0800

    update uoj_test12-3

---
+++ src/uoj_test12-3.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+int main(int argc, char const *argv[])
+{
+	int n,m;
+	scanf("%d%d",&m,&n);
+	int sum=1;
+	for (int i = m; i > n; --i)
+	{
+		sum*=i;
+	}
+	printf("%d",sum);
+	return 0;
+}
