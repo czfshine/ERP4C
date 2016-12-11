8ed247296f845cbb86a187099ec6442a095de641
czfshine <czfshine@outlook.com>
2016-12-11 20:36:19 +0800

    update uoj_newbaby_C

--- src/uoj_newbaby_C.c
+++ src/uoj_newbaby_C.c
@@ -22,41 +22,28 @@ int main(int argc, char const *argv[])
 		B[cur]='\0';
 
 		scanf("%d",&D);
-		printf("%d%s%d\n\n",A,B,D);
+		//printf("%d%s%d\n\n",A,B,D);
 
 		int len=strlen(B);
-
+		printf("%d",A);
 		if(len>D){
 			int sub=len-D;
-			printf("%d",A);
 			int i;
 			for (i = 0; i < D; ++i)
-			{
 				printf("%c",B[i]);
-			}
 			printf(".");
 			for (int j = 0; j < sub; ++j)
-			{
 				printf("%c",B[D+j]);
-			}
 		}else if(len==D){
-			printf("%d",A);
 			for (int i = 0; i < D; ++i)
-			{
 				printf("%c",B[i]);
-			}
 		}else{
 			int sub=D-len;
-			printf("%d",A);
 			int i;
 			for (i = 0; i < len; ++i)
-			{
 				printf("%c",B[i]);
-			}
 			for (int j = 0; j < sub; ++j)
-			{
 				printf("0");
-			}
 		}
 		printf("\n");
 	}
