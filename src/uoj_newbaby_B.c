b132dd87768c79f8ea5110900634967892c0f1fa
czfshine <czfshine@outlook.com>
2016-12-11 22:50:49 +0800

    update uoj_newbaby_B

--- src/uoj_newbaby_B.c
+++ src/uoj_newbaby_B.c
@@ -1,12 +1,27 @@
 #include "stdio.h"
-int a,b,c;
+/*
+ 测试数据用例 No.2
 
-void sort(){
-	int t;
-	if(a<b){t=a,a=b,b=t;}
-	if(b<c){t=b,b=c,c=t;}
-	if(a<c){t=a,a=c,c=t;}
-}
+标准输入数据:
+22 4
+57 27
+61 3
+5 4
+   1|6
+   2|4
+   3|9
+   4|3
+
+
+你的错误输出结果:
+   1|8
+   2|4
+   3|31
+   4|3
+*/
+
+/*
+
 int main(int argc, char const *argv[])
 {
 	int n,m,count=0;
@@ -26,5 +41,33 @@ int main(int argc, char const *argv[])
 		//printf("%d%d%d",a,b,c);
 		printf("%d\n",count);
 	}
+	return 0;
+}
+*/
+int a,b,c;
+
+void sort(){
+	int t;
+	if(a<b){t=a,a=b,b=t;}
+	if(b<c){t=b,b=c,c=t;}
+	if(a<c){t=a,a=c,c=t;}
+}
+
+int main(int argc, char const *argv[])
+{
+	int n,m,count=0;
+
+	while(scanf("%d%d",&n,&m)>0){
+		count=0;
+		a=b=c=m;
+		while(a!=n){
+			printf("%d%d%d",a,b,c);
+			a=min(b+c-1,n);
+			sort();
+		}
+
+
+	}
+
 	return 0;
 }
