a6e2696761e934bac215e8af6b72185bfc1d910a
czfshine <czfshine@outlook.com>
2016-12-12 15:43:59 +0800

    update uoj_newbaby_A.python

--- src/uoj_newbaby_A.python.c
+++ src/uoj_newbaby_A.python.c
@@ -1,10 +1,10 @@
 //23333333333333333333
 #include "stdio.h"
-#define ): )
+#define or ||
 int main(int argc, char const *argv[])
 {	int n;
 	scanf("%d",&n);
-	if(n==4||n==2||n==1)
+	if(n==4 or n==2 or n==1)
 		printf("NO");
 	else
 		printf("%d %d",(n%2==0)?(n*n/4-1):(n*n/2),(n%2==0)?(n*n/4+1):(n*n/2+1));
