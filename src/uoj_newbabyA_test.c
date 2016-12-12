7e0f16c6cbcf8e5a0eb6779cb0301324bd6bc83b
czfshine <czfshine@outlook.com>
2016-12-12 08:52:35 +0800

    update uoj_newbabyA_test

--- src/uoj_newbabyA_test.c
+++ src/uoj_newbabyA_test.c
@@ -1,6 +1,7 @@
 #include "stdio.h"
 #include "stdlib.h"
-void f(int n){
+#include "string.h"
+void f(int n,char * str){
 	long long  a,flag=0;
 	a=n*n;
 	for (long long  i =1; i < a; i++)
@@ -12,7 +13,7 @@ void f(int n){
 			long long b=abs((i-a/i)/2);
 			if(b*b+a==c*c&& b!=0)
 				if((n<b&&n<c)&&(n+b>c&&n+c>b&&b+c>n)){
-					printf("%lld %lld",b,c);
+					sprintf(str,"%lld %lld",b,c);
 					flag=1;
 					break;
 				}
@@ -22,31 +23,33 @@ void f(int n){
 	if(!flag) printf("N0");
 }
 
-void s (int n){
+void s (int n,char * str){
 	if(n>=5){
 	if(n%2==0){
-		printf("%d %d",n*n/4-1,n*n/4+1);
+		sprintf(str,"%d %d",n*n/4-1,n*n/4+1);
 	}else{
-		printf("%d %d",n*n/2,n*n/2+1);
+		sprintf(str,"%d %d",n*n/2,n*n/2+1);
 	}}
 	else{
 		if(n==4||n==2||n==1){
-			printf("NO");
+			sprintf(str,"NO");
 		}else
 		{
-			printf("4 5");
+			sprintf(str,"4 5");
 		}
 	}
 }
 int main(int argc, char const *argv[])
-{
+{	char s1[100],s2[100];
 	for (int i = 1; i < 10001; ++i)
 	{
-		printf("----%d----",i);
-		f(i);
-		printf("\n");
-		s(i);
-		printf("\n");
+		//printf("----%d----",i);
+		f(i,s1);
+//printf("\n");
+		s(i.s2);
+		//、、printf("\n");
+		if(strcmp(s1,s2)!==0)
+				printf("%d",i);
 	}
 	return 0;
 }
