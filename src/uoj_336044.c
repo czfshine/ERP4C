fe8385f3ba689a645d41d119baaacc6485ef4669
czfshine <czfshine@outlook.com>
2016-12-14 17:59:27 +0800

    update uoj_336044

---
+++ src/uoj_336044.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+
+#include "string.h"
+
+void rev(char *str ,int len){
+		for (int i = 0; i < len/2; ++i)
+		{	char t=str[i];
+			str[i]=str[len-i-1];
+			str[len-i-1]=t;
+		}
+	
+}
+int main(int argc, char const *argv[])
+{
+	char data[101]="123";
+	rev(data);
+	printf("%s",data);
+	int sum;
+
+	return 0;
+}
