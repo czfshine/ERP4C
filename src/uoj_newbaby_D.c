194f97b5af323b116e7997b87f707dffe25c9246
czfshine <czfshine@outlook.com>
2016-12-14 12:47:33 +0800

    update uoj_newbaby_D

--- src/uoj_newbaby_D.c
+++ src/uoj_newbaby_D.c
@@ -1,54 +1,15 @@
 #include "stdio.h"	
+#include <stdlib.h>
 int main(int argc, char const *argv[])
 {
 	int n;
 	scanf("%d",&n);
-	n++;
-	for (int i = 0; i < n; ++i)
+	for (int i = -n; i < n; ++i)
 	{
-		for (int j = 1; j < n-i; ++j)
-			printf(" ");
-		    printf("*");
-		for (int k = 0; k < i*2+1-2 ; ++k)
-			printf(" ");
-			if(i!=0) printf("*");
-			for (int j = 0; j < i; ++j)
-			printf(" ");
-
-
-		for (int j = 1; j < n-i; ++j)
-			printf(" ");
-		    printf("*");
-		for (int k = 0; k < i*2+1-2 ; ++k)
-			printf(" ");
-			if(i!=0) printf("*");
-			for (int j = 0; j < i+1; ++j)
-			printf(" ");
-
-
-		printf("\n");
-	}
-	for (int i = 0; i < n-1; ++i)
-	{
-		for (int j = 0; j < i+1; ++j)
-			printf(" ");
-			printf("*");
-		for (int k = 3; k < (n-i)*2-2 ; ++k)
-			printf(" ");
-			if(i!=n-2)printf("*");
-			for (int j = 0; j < i+2; ++j)
-			printf(" ");
-
-		for (int j = 0; j < i+1; ++j)
-			printf(" ");
-			printf("*");
-		for (int k = 3; k < (n-i)*2-2 ; ++k)
-			printf(" ");
-			if(i!=n-2)printf("*");
-			for (int j = 0; j < i+2; ++j)
-			printf(" ");
-		
-		printf("\n");
+		for (int i = 0; i < abs(n); ++i)
+		{
+			/* code */
+		}
 	}
 	return 0;
 }
