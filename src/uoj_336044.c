6ef45b2f5ca160eab5166cd7f04ec5462d77e157
czfshine <czfshine@outlook.com>
2016-12-14 18:02:17 +0800

    update uoj_336044

--- src/uoj_336044.c
+++ src/uoj_336044.c
@@ -14,7 +14,7 @@ int main(int argc, char const *argv[])
 {
 	char data[101]="12g3";
 	int sum=0,n;
-	while(scnaf("%s",data)>0){
+	while(scanf("%s",data)>0){
 		rev(data,strlen(data)-1);
 		sscanf(data,"%d",&n);
 		sum+=n;
