1336210cbcec8ddea684e833a0e54767dfe9a004
czfshine <czfshine@outlook.com>
2016-12-14 17:47:31 +0800

    update uoj_336043

--- src/uoj_336043.c
+++ src/uoj_336043.c
@@ -10,9 +10,9 @@ main()
         { 
 
             if(j>i) 
-                a[i][j] = 2-abs(i-m); 
+                a[i][j] = m+2-abs(i-m); 
             else 
-                a[i][j] = 2-abs(j-m); 
+                a[i][j] = m+2-abs(j-m); 
 
         } 
 for(i=0; i<n; i++) 
