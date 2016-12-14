ce28778fe6f4620ebe2955bb22157cccc7471eaf
czfshine <czfshine@outlook.com>
2016-12-14 17:17:08 +0800

    update uoj_336036

--- src/uoj_336036.c
+++ src/uoj_336036.c
@@ -15,6 +15,6 @@ int main(int argc, char const *argv[])
 	if(p<d*r ||p/(p-d*r)<1)
 		printf("God\n");
 	else
-		printf("%.1lf",log(p/(p-d*r))/log(1+r));
+		printf("%.1f",log(p/(p-d*r))/log(1+r));
 	return 0;
 }
