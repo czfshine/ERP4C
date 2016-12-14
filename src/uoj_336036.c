ddd0f7df9a40678075734a7b291241a66a431288
czfshine <czfshine@outlook.com>
2016-12-14 17:16:06 +0800

    update uoj_336036

---
+++ src/uoj_336036.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+#include "math.h"
+
+int main(int argc, char const *argv[])
+{
+	double d,p,r;
+
+	scanf("%lf%lf%lf",&d,&p,&r)
+
+	if(d==0)
+		printf("0.0\n");
+
+	if(p<d*r ||p/(p-d*r)<1)
+		printf("God\n");
+	else
+		printf("%.1lf",log(p/(p-d*r))/log(1+r));
+	return 0;
+}
