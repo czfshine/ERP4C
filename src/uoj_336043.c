1600c59870f8d9640c52e83d5c4d46c1b77c3baa
czfshine <czfshine@outlook.com>
2016-12-14 17:43:15 +0800

    update uoj_336043

---
+++ src/uoj_336043.c
@@ -0,0 +1,27 @@
+#include "stdio.h" 
+#include "stdlib.h" 
+#include "math.h" 
+main() 
+{    int a[20][20], n, i, j, m; 
+    scanf("%d", &n); 
+    m=n/2; 
+    for(i=0; i<n; i++) 
+        for(j=0; j<n; j++) 
+        { 
+/********************************************************** 
+            if(j>i) 
+                a[i][j] = m+2-abs(i-m+1); 
+            else 
+                a[i][j] = m+2-abs(j-m+1); 
+**********************************************************/ 
+             if(j>i) 
+                a[i][j] = m+2-abs(i-m+1); 
+            else 
+                a[i][j] = m+2-abs(j-m+1); 
+        } 
+for(i=0; i<n; i++) 
+{    for(j=0; j<n; j++) 
+            printf("%d ", a[i][j]); 
+        printf("\n"); 
+    } 
+} 
