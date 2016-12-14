47248909c0c613c5ec5357700bb1269b16d13e86
czfshine <czfshine@outlook.com>
2016-12-14 23:54:33 +0800

    update uoj_newbabyE

--- src/uoj_newbabyE.c
+++ src/uoj_newbabyE.c
@@ -12,7 +12,7 @@ int main(int argc, char const *argv[])
 		temp=a/(4*N)+2;
 
 		while(4*N*temp-temp*temp >a){
-			printf("%lld",4*N*temp-temp*temp);
+			printf("%lld\n",4*N*temp-temp*temp);
 			temp--;
 		}
 		printf("%lld",temp+1);
