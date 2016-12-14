e6c89f6255f4b3b8516ddd9e51c731db403bb273
czfshine <czfshine@outlook.com>
2016-12-14 21:19:20 +0800

    update uoj1109

--- src/uoj1109.cpp
+++ src/uoj1109.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 map <string ,int> Dict;
+bool end;
 //debug
 #ifdef LOCAL
 #define fp stdin 
@@ -32,6 +33,7 @@ string getword(){
 		else
 			break;
 	}
+	if(ch==EOF) end=true;
 	return s;
 }
 int main(int argc, char const *argv[])
