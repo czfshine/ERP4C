036a3e6c036d7076516d2fd99ef8c565ee1072e5
czfshine <czfshine@outlook.com>
2016-12-14 12:55:21 +0800

    update uoj_newbaby_D

--- src/uoj_newbaby_D.c
+++ src/uoj_newbaby_D.c
@@ -7,34 +7,19 @@ int main(int argc, char const *argv[])
 	for (int i = -n; i <= n; ++i)
 	{
 		for (int j = 0; j < abs(i); ++j)
-		{
 			printf(" ");
-		}
-
 		printf("*");
-
 		if(abs(i)!=n)
 			for (int j= 0; j < (n-abs(i))*2-1; ++j)
-			{
 				printf(" ");
-			}
-
 		if(abs(i)!=n) printf("*");
-
 		for (int j = 0; j < abs(i)*2-1; ++j)
-		{
 			printf(" ");
-		}
 		if(i) printf("*");
-
-if(abs(i)!=n)
+		if(abs(i)!=n)
 			for (int j= 0; j < (n-abs(i))*2-1; ++j)
-			{
 				printf(" ");
-			}
-
 		if(abs(i)!=n) printf("*");
-
 		printf("\n");
 	}
 	return 0;
