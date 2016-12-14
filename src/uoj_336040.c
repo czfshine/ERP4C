617244d2a2e3f2b00f470953c1f946cd4c42ad32
czfshine <czfshine@outlook.com>
2016-12-14 17:29:32 +0800

    update uoj_336040

---
+++ src/uoj_336040.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+#include <stdlib.h>
+int main(int argc, char const *argv[])
+{
+	int n;
+	scanf("%d",n);
+	for (int i = -n; i < =n; ++i)
+	{
+		for (int j = 0; j < abs(n); ++j)
+		{
+			printf(" ");
+		}
+		printf("%d",n-abs(i)+1);
+
+		if(abs(i)!=n)
+			for (int j = 0; j < (n-abs(i))*2-1; ++j)
+			{
+				printf(" ");
+			}
+			printf("%d",n-abs(i)+1);
+	}
+	return 0;
+}
