aaab113e88ce886adeb35d42f8ea7002ee0d88cc
czfshine <czfshine@outlook.com>
2016-12-14 18:05:49 +0800

    update uoj_336044

--- src/uoj_336044.c
+++ src/uoj_336044.c
@@ -3,6 +3,13 @@
 #include "string.h"
 
 void rev(char *str ,int len){
+	if(len%2==0)
+		for (int i = 0; i < len/2; ++i)
+		{	char t=str[i];
+			str[i]=str[len-i-1];
+			str[len-i-1]=t;
+		}
+		else
 		for (int i = 0; i < len/2; ++i)
 		{	char t=str[i];
 			str[i]=str[len-i-1];
@@ -15,7 +22,7 @@ int main(int argc, char const *argv[])
 	char data[101]="12g3";
 	int sum=0,n;
 	while(scanf("%s",data)>0){
-		rev(data,strlen(data)-1);
+		rev(data,strlen(data));
 		sscanf(data,"%d",&n);
 		printf("%s",data);
 		sum+=n;
