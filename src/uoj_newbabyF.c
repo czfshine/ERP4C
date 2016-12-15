3d72db04180f226a013925b02797019dde8eb9d3
czfshine <czfshine@outlook.com>
2016-12-15 12:35:21 +0800

    update uoj_newbabyF

--- src/uoj_newbabyF.c
+++ src/uoj_newbabyF.c
@@ -3,15 +3,27 @@
 #define min(x,y) x<y?x:y
 char  str[31];
 int end=0;
+int vis[31];
+
+
+void print(int s,int e){
+
+	for (int i = s; i < e; ++i)
+	{
+		putchar(str[i]);
+	}
+
+	printf("\n");
+}
 int find(int s){
 	printf("%d %s\n",s,str+s);
 	if(s==end) return 1+1;//print("%s",str+s);
-	for (int i = s+1; i < (min(end,s+5)); ++i)
+	for (int i = s+1; i < (min(end,s+6)); ++i)
 	{
-		find(i);
+		vis[i]=find(i);
 	}
 	if(end-s>6)
-		find(s+6);
+		vis[s+6]=find(s+6);
 }
 int main(int argc, char const *argv[])
 {	
