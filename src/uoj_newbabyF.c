3507a69dc6c266f6708fb3355112efd4f9cc5b10
czfshine <czfshine@outlook.com>
2016-12-15 09:04:02 +0800

    update uoj_newbabyF

--- src/uoj_newbabyF.c
+++ src/uoj_newbabyF.c
@@ -1,20 +1,20 @@
 #include "stdio.h"
 
-char * str[31]="12345678";
+char * str[]="12345678";
 int end=7;
 int find(int s){
-	print("%s",str+s);
+	printf("%s",str+s);
 	if(s==end) 1+1//print("%s",str+s);
 	for (int i = s; i < min(end,s+6); ++i)
 	{
 		find(i);
 	}
-	if(e-s>6)
+	if(end-s>6)
 		find(s+6);
 }
 int main(int argc, char const *argv[])
 {	
-	find("12345678",0);
+	find(0);
 
 	return 0;
 }
