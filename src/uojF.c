806732e1f54d44e08912088bff8947b64c4945f4
czfshine <czfshine@outlook.com>
2016-12-15 19:03:10 +0800

    update uojF

--- src/uojF.c
+++ src/uojF.c
@@ -1,26 +1,12 @@
-#include <string> 
-#include <iostream> 
-#include <vector> 
-using namespace std; 
-void split(vector<string> &comb, string s) { 
-	if (s == "") { 
-		for(int j = 0; j < comb.size(); j++)
-			cout << comb[j];
-		cout <<" "<< endl; 
-	} 
-	for (unsigned i = 1; i <= s.size(); ++i) { 
-		comb.push_back(s.substr(0, i)); 
-		
-		split(comb, s.substr(i)); 
-		
-		comb.pop_back();
-	}
-} 
-int main() { 
-	string s; 
-	cin >> s; 
-	vector<string> combination; 
-	
-	split(combination, s); 
-	return 0; 
+#include "stdio.h"
+#include "string.h"
+
+#define min(x,y) x<y?x:y
+
+char  str[31];
+int end=0;
+
+
+int find(int cur){
+
 }
