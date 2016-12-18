8378f9f2867d24fc388263ce25337dd02568e410
czfshine <czfshine@outlook.com>
2016-12-18 11:52:12 +0800

    update uoj1109_array

--- src/uoj1109_array.c
+++ src/uoj1109_array.c
@@ -191,23 +191,11 @@ main()
 	char tmp_w[21];
 
 	FILE * infile;
-	 #ifdef LOCAL
-    	infile=stdin;
-    #else
-   		infile=open(filename);
-    #endif
+	infile=open(filename);
 
-    while(next(infile,tmp_w))
-        if(strlen(tmp_w)>0)
-            counter(tmp_w);
-    #ifdef LOCAL
-    printf("input&count test:\n");
-    print();
-    sortk_v();
-    printf("sort test\n");
-    print();
-    printf("output test\n");
-    #endif
+	while(next(infile,tmp_w))
+		if(strlen(tmp_w)>0)
+			counter(tmp_w);
 
 	sortk_v();
 	output();
