1b37c2a5b251699c388c19f3a3cc66c9d73fbf1e
czfshine <czfshine@outlook.com>
2016-12-18 08:41:27 +0800

    update uoj1109_array

--- src/uoj1109_array.c
+++ src/uoj1109_array.c
@@ -58,8 +58,10 @@ FILE* open(char * filename){
 int canfind(char * word ){
 	int i=0;
 	while(words[i][0]!='\0' &&(strcmp(words[i++],word)!=0 )){
-		printf("%s,%s,%d\n",words[i],word,i);
+		//printf("%s,%s,%d\n",words[i],word,i);
 	}
+
+	printf("%s,%s,%d\n",words[i],word,i);
 	if(words[i][0]=='\0'){
 		return 0;
 	}else{
