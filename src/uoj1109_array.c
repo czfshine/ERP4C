70479ef44301765d779c117ee4d94d3b931cc3e5
czfshine <czfshine@outlook.com>
2016-12-18 08:25:24 +0800

    update uoj1109_array

--- src/uoj1109_array.c
+++ src/uoj1109_array.c
@@ -63,7 +63,7 @@ void counter(char * word){
 	printf("%s\n",word);
 	int index=0;
 
-	printf("%s,%d",word,canfind(word));
+	printf("%s,%d\n",word,canfind(word));
 	if(index=canfind(word)){
 		count[index]++;
 	}else{
