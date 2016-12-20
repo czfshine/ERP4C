2169b7a5ea1e2c7df21cb5b99fe90b19445bdfad
czfshine <czfshine@outlook.com>
2016-12-20 14:10:22 +0800

    update uoj1109_withoutsort

--- src/uoj1109_withoutsort.c
+++ src/uoj1109_withoutsort.c
@@ -12,7 +12,10 @@ int next(FILE * infile,char * word){
             word[i++]=tolower(ch);
         else{
         	if(ch=='-')
-            	((ch=getc(infile))=='\n')?continue;:fseek(infile,-1L,SEEK_CUR);
+            	if((ch=getc(infile))=='\n')
+                	continue;
+            	else
+                	fseek(infile,-1L,SEEK_CUR);
 			word[i]='\0';
            	return 1;
     	}
