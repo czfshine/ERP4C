b80e7bdcde50d90a022629c749c7e3fe3f85c043
czfshine <czfshine@outlook.com>
2017-03-09 22:38:21 +0800

    update freopen

---
+++ src/freopen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	freopen("test.txt","r",stdout);
+
+	srand(1)
+
+	for (int i = 0; i < 100; ++i)
+	{
+		printf("%d %d",rand(),rand());
+	}
+	return 0;
+}
