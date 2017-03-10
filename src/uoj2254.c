2c26c8edc4b019e5f54fa20a5e0de981294ba037
czfshine <czfshine@outlook.com>
2017-03-10 15:26:06 +0800

    update uoj2254

---
+++ src/uoj2254.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <queue>
+char name[100000][20];
+char u[100000];
+char ed[100000];
+
+
+int main(int argc, char const *argv[])
+{
+	int n;
+	scanf("%d",&n);
+
+	int maxmin;
+	scanf("%d",&maxmin);
+
+	int usern;
+	if (maxmin/5){
+
+		usern=(maxmin/5) +1;
+	}else
+	{
+		usern=maxmin/5;
+	}
+
+
+	for (int i = 0; i < maxmin; ++i)
+	{
+		
+	}
+	return 0;
+}
