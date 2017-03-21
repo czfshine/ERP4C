65804c4bfb4e7b8546f0400275c9ab0a34369613
czfshine <czfshine@outlook.com>
2017-03-21 10:07:47 +0800

    update uoj8576

---
+++ src/uoj8576.cpp
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<malloc.h>
+#define OK 1 
+#define ERROR 0
+#define LIST_INIT_SIZE 100
+#define LISTINCREMENT 10
+#define ElemType int
+
+typedef struct
+{
+	int *elem;
+	int length;
+	int listsize;
+}SqList;
+
+int InitList_Sq(SqList &L)
+{
+// 算法2.3，构造一个空的线性表L，该线性表预定义大小为LIST_INIT_SIZE
+// 请补全代码
+
+}
+
+int Load_Sq(SqList &L)
+{
+// 输出顺序表中的所有元素
+	int i;
+	if(_________________________) printf("The List is empty!");  // 请填空
+	else
+	{
+		printf("The List is: ");
+		for(_________________________) printf("%d ",_________________________);  // 请填空
+	}
+	printf("\n");
+	return OK;
+}
+
+int ListInsert_Sq(SqList &L,int i,int e)
+{
+// 算法2.4，在顺序线性表L中第i个位置之前插入新的元素e
+// i的合法值为1≤i≤L.length +1
+// 请补全代码
+
+}
+
+int ListDelete_Sq(SqList &L,int i, int &e)
+{
+// 算法2.5,在顺序线性表L中删除第i个位置的元素，并用e返回其值
+// i的合法值为1≤i≤L.length
+// 请补全代码
+
+}
+
+int main()
+{
+	SqList T;
+	int a, i;
+	ElemType e, x;
+	if(_________________________)    // 判断顺序表是否创建成功
+	{
+		printf("A Sequence List Has Created.\n");
+	}
+	while(1)
+	{
+		printf("1:Insert element\n2:Delete element\n3:Load all elements\n0:Exit\nPlease choose:\n");
+		scanf("%d",&a);
+		switch(a)
+		{
+			case 1: scanf("%d%d",&i,&x);
+					if(_________________________) printf("Insert Error!\n"); // 判断i值是否合法，请填空
+					else printf("The Element %d is Successfully Inserted!\n", x); 
+					break;
+			case 2: scanf("%d",&i);
+					if(_________________________) printf("Delete Error!\n"); // 判断i值是否合法，请填空
+					else printf("The Element %d is Successfully Deleted!\n", e);
+					break;
+			case 3: Load_Sq(T);
+					break;
+			case 0: return 1;
+		}
+	}
+}
