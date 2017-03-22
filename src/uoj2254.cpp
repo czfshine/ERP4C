0555a610ff690413d0337d7c673bbc8a7bbed15f
czfshine <czfshine@outlook.com>
2017-03-22 12:19:54 +0800

    update uoj2254

--- src/uoj2254.cpp
+++ src/uoj2254.cpp
@@ -81,7 +81,7 @@ int main(int argc, char const *argv[])
 		int time;
 		scanf("%d%d",&usernum,&time);
 
-		for (int i = 0; i <= time && i< usernum *5; ++i)
+		for (int i = 0; i <= time ; ++i)
 		{	
 			read(i);
 			//cout<<i<<"\n"<<lv1.size()<<lv2.size()<<lv3.size()<<endl;
