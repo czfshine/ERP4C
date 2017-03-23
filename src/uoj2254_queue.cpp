b3bec55b6a483c313749beb29d36f780c581e095
czfshine <czfshine@outlook.com>
2017-03-23 11:08:22 +0800

    update uoj2254_queue

--- src/uoj2254_queue.cpp
+++ src/uoj2254_queue.cpp
@@ -1,1 +1,41 @@
-uoj2254_queue.cpp
+include <stdio.h>
+
+main(int argc, char const *argv[])
+{
+	int n;
+	scanf("%d",&n);
+	int lv[n];
+	for (int i = 0; i < n; ++i)
+	{
+		/* code */
+
+		scanf("%d",data+i);
+	}
+
+
+	int f[10000];
+	int s[10000];
+
+	char ch;
+	int *p=f;
+	int cur=0;
+	while(scanf("%c",ch)>0){
+
+		if(ch=='='){
+			p=s;
+			cur=0;
+		}
+
+		for (int i = 0; i < data[ch-'a']; ++i)
+		{
+			p[cur+i]=ch*10000+i;
+			cur++;
+		}
+	}
+
+	for (int i = 0; i < cur; ++i)
+	{
+		printf("%d",f+i);
+	}
+	return 0;
+}
--- src/uoj346478.c
+++ src/uoj346478.c
@@ -1,38 +1,48 @@
 #include <stdio.h>
+
 int main(int argc, char const *argv[])
 {
 	int n,s,m;
-
-	scanf("%d %d %d",&n,&s,&m);
+	scanf("%d%d%d",&n,&s,&m);
 	char data[n];
 
 	for (int i = 0; i < n; ++i)
 	{
+		//n 写错
 		data[i]=1;
 	}
-
-	int l=0,d=1 ,cur;
-
+	if(m==1){
+		for (int i = s; i <= n; ++i)
+		{
+			printf("%d ",i);
+		}
+		for (int i = 1; i < s; ++i)
+		{
+			printf("%d ",i);
+		}
+		return 0;
+	}
+	int l=0,d=1,cur;
 	l=n;
 	cur=s-1;
-
 	while(l>=1){
-		int now=(cur+1)%n;
+
 		for(;;){
-			
-			if(data[now]){
+			if(data[(cur+1)%n]){
 				d++;
 				if((d%m)==0){
-					data[now]=0;
-					printf("%d ",now+1);
-					cur=now;
+					data[(cur+1)%n]=0;
+					printf("%d ",(cur+1)%n+1);
+					cur=(cur+1)%n;
 					l--;
 					break;
-
 				}
+
 			}
-			cur=now;
+			cur=(cur+1)%n;
 		}
 	}
+
+	//printf("%d",data[(cur+1)%n]);
 	return 0;
 }
