344b5da8725ff5c4eed1b7319ec316ab50ced282
czfshine <czfshine@outlook.com>
2017-03-27 19:44:43 +0800

    update 346588

---
+++ src/346588.cpp
@@ -0,0 +1,36 @@
+#include <iossteam>
+
+#include <queue>
+#include <utility>
+
+typedef pair<int,int> node;
+
+
+int re[1000000]
+int main(int argc, char const *argv[])
+{
+	priority_queue<node> pq;
+
+	pq.push(make_pair(1,2));
+	node n;
+	while(1){
+
+		node n=pq.front();
+
+		pq.pop();
+
+		switch(n.second){
+			case 2:
+				pq.push(make_pair(n.first*2,3));
+			break;
+			case 3:
+				pq.push(make_pair(n.first*3,5);
+			break;
+			case 5:
+				pq.push(make_pair(n.first*5,0));
+			break;
+		}
+
+	}
+	return 0;
+}
