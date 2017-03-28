e40d6c46b8a8760dcfab2f1eb70684be978d3e6e
czfshine <czfshine@outlook.com>
2017-03-28 11:44:47 +0800

    update hdu1001

--- src/hdu1001.cpp
+++ src/hdu1001.cpp
@@ -4,11 +4,14 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	int n;
-	
+	int f=0;
 	int s=0,a;
 	while(cin >>n){
 		s=(n+1)*n/2;
-		cout<<s<<endl<<endl;
+		if(f!=0)cout<<endl;
+		cout<<s<<endl;
+
+		f++;
 	}
 	return 0;
 }
---
+++ src/hdu1002.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+	int n;
+	cin >>n;
+	char a[200];
+	string num1,num2,out,temp;
+
+	while(n--){
+		cin>>num1>>temp>>num2;
+
+		num.r
+	}
+	return 0;
+}
