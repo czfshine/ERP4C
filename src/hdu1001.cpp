d2dfb2f5325affc1566076a58704ba90261c5161
czfshine <czfshine@outlook.com>
2017-03-28 11:02:50 +0800

    update hdu1001

---
+++ src/hdu1001.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int n;
+	cin >>n;
+	int s=0,a;
+	while(n--){
+		cin>>a;
+		s+=a;
+	}
+	cout<<a;
+	return 0;
+}
