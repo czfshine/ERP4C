1dafa426c1708eb3afab81267e63f35f2f7c4a07
czfshine <czfshine@outlook.com>
2017-03-31 21:10:23 +0800

    update uoj10571

--- src/uoj10571.cpp
+++ src/uoj10571.cpp
@@ -6,20 +6,20 @@ int main(int argc, char const *argv[])
 {
 	char ch;
 	while(1){
-	while((ch=getchar())!=EOF){
+		while((ch=getchar())!=EOF){
 
-		if(ch==':') break;
-	}
-	getchar();getchar();
+			if(ch==':') break;
+		}
+		getchar();getchar();
 
-	while((ch=getchar())!=EOF){
-		
-		if(ch==':') break;
-		if(ch=='/') break;
-		putchar(ch);
+		while((ch=getchar())!=EOF){
+			
+			if(ch==':') break;
+			if(ch=='/') break;
+			putchar(ch);
+		}
+		putchar('\n');
+		while((ch=getchar())!=EOF)if(ch=='\n') break;
 	}
-	putchar('\n');
-	while((ch=getchar())!=EOF)if(ch=='\n') break;
-}
 	return 0;
 }
