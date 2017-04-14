7452bd2593c510e325f29798b2801b415d0acd6b
czfshine <czfshine@outlook.com>
2017-04-14 18:29:04 +0800

    update hdu2571

--- src/hdu2571.cpp
+++ src/hdu2571.cpp
@@ -36,23 +36,60 @@ const double FINF = 1e18;
 #define print(x) cout<<(x)<<endl;
 #define IOS ios::sync_with_stdio(0);cin.tie(0);
 
-
+int a[20][1005];
+int dp[25][1005];
+int n,m;
 void Init(){
 
+	
+	scanf("%d%d",&n,&m);
+
+	REP(i,1,n){
+		REP(j,1,m){
+
+			scanf("%d",&a[i][j]);
+		}
+	}
 
 
 	return ;
 }
 
 void Solve(){
+	REP(i,0,m-1)
+		dp[0][i]=-105;
+	REP(i,0,n-1)
+		dp[i][0]=-105;
 
+
+	dp[0][1]=dp[1][0]=0;
+
+	REP(i,1,n)
+	REP(j,1,m)
+	{
+
+		dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+
+		REP(k,2,m){
+			if(j%k==0){
+
+				dp[i][j]=max(dp[i][j],dp[i][j/k]);
+			}
+
+		}
+
+		dp[i][j]+=a[i][j];
+	}
+	print(dp[n][m]);
 	return ;
 }
 
 int main(){
 
 	freopen("data.in","r",stdin);
-
+	int T ;
+	scanf("%d",&T);
+	while(T--)
 	/* code */
 	Init(),Solve();
 	return 0;
