737f3f6c0a6f10f6062c6c35928a22e78a6fe195
czfshine <czfshine@outlook.com>
2017-04-14 22:21:24 +0800

    update hdu2084

---
+++ src/hdu2084.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <cmath>
+#include <list>
+#include <bitset>
+
+using namespace std;
+
+#define ll long long 
+#define lson l,mid,id<<1
+#define rson mid+1,r,id<<1|1
+
+typedef pair<int, int>pii;
+typedef pair<ll, ll>pll;
+typedef pair<double, double>pdd;
+
+const double eps = 1e-6;
+const ll  LINF = 0x3f3f3f3f3f3f3f3fLL;
+const int INF = 0x3f3f3f3f;
+const double FINF = 1e18;
+
+#define x first
+#define y second
+
+#define REP(i,j,k) for(int i =(j);i<=(k);i++)
+#define REPD(i,j,k) for(int i =(j);i>=(k);i--)
+#define print(x) cout<<(x)<<endl;
+#define IOS ios::sync_with_stdio(0);cin.tie(0);
+
+
+
+
+void Init(){
+
+
+	return ;
+}
+
+void Solve(){
+	
+
+	return ;
+}
+
+int main(){
+
+	freopen("hdu2084.in","r",stdin);
+
+
+
+	Init(),Solve();
+	return 0;
+}
