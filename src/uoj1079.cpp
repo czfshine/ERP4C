a02879af176df6cfe76aaa96a8759478e15251e4
czfshine <czfshine@outlook.com>
2017-04-17 19:32:51 +0800

    update uoj1079

---
+++ src/uoj1079.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <cmath>
+#include <list>
+#include <bitset>
+
+using namespace std;
+
+#define ll long long 
+#define lson l,mid,id<<1
+#define rson mid+1,r,id<<1|1
+
+typedef pair<int, int>pii;
+typedef pair<ll, ll>pll;
+typedef pair<double, double>pdd;
+
+const double eps = 1e-6;
+const ll  LINF = 0x3f3f3f3f3f3f3f3fLL;
+const int INF = 0x3f3f3f3f;
+const double FINF = 1e18;
+
+#define x first
+#define y second
+
+#define REP(i,j,k) for(int i =(j);i<=(k);i++)
+#define REPD(i,j,k) for(int i =(j);i>=(k);i--)
+#define print(x) cout<<(x)<<endl;
+#define IOS ios::sync_with_stdio(0);cin.tie(0);
+
+
+
+
+void Init(){
+
+
+	return ;
+}
+
+void Solve(int t){
+	int n;
+	scanf("%d",&n);
+
+	int a=n*n;
+
+	REPD(i,2051,3){
+
+		int b=i*i;
+
+		int k1=sqrt(a+b);
+			if(k1*k1+b==a&& k1>0){
+				print("%d %d %d\n",max(i,k1),min(i,k1));
+			}
+		int k2=sqrt(a-b);
+			if(k2*k2+b==a&& k2>0){
+				print("%d %d %d\n",max(i,k2),min(i,k2));
+			}
+		int k3=sqrt(b-a);
+			if(k3*k3+b==a&& k1>0){
+				print("%d %d %d\n",max(i,k3),min(i,k3));
+			}
+	}
+	if(t>1) printf("\n");
+
+	return ;
+}
+
+int main(){
+
+	freopen("uoj1079.in","r",stdin);
+
+
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	Init(),Solve(t);
+	return 0;
+}
