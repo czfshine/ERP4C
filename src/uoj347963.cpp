8325c93039a948f96673a5f61f54d0aa1ecc0409
czfshine <czfshine@outlook.com>
2017-04-20 10:24:16 +0800

    update uoj347963

---
+++ src/uoj347963.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <cmath>
+#include <list>
+#include <bitset>
+
+using namespace std;
+
+#define ll long long 
+#define lson l,mid,id<<1
+#define rson mid+1,r,id<<1|1
+
+typedef pair<int, int>pii;
+typedef pair<ll, ll>pll;
+typedef pair<double, double>pdd;
+
+const double eps = 1e-6;
+const ll  LINF = 0x3f3f3f3f3f3f3f3fLL;
+const int INF = 0x3f3f3f3f;
+const double FINF = 1e18;
+
+#define x first
+#define y second
+
+#define REP(i,j,k) for(int i =(j);i<=(k);i++)
+#define REPD(i,j,k) for(int i =(j);i>=(k);i--)
+#define print(x) cout<<(x)<<endl;
+#define IOS ios::sync_with_stdio(0);cin.tie(0);
+
+
+string in;
+
+
+void Init(){
+
+	cin>>in;
+	return ;
+}
+
+void Solve(){
+	
+	int len=in.size():
+
+	REP(k,1,len/2){
+
+		if(in[k-1]!='#'){
+			cout<<in[2*k-1]<<in[2*k]<<endl;
+		}
+
+	}
+	return ;
+}
+
+int main(){
+
+	freopen("uoj347963.in","r",stdin);
+
+	Init(),Solve();
+	return 0;
+}
