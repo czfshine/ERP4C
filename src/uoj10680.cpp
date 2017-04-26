7e073f634665d4c140c3801f2550e2cbb58660da
czfshine <czfshine@outlook.com>
2017-04-26 21:52:38 +0800

    update uoj10680

--- src/uoj10680.cpp
+++ src/uoj10680.cpp
@@ -70,7 +70,7 @@ void Solve(){
 	int ansa,ansb;
 	REP(i,mina,f)
 	{
-		REP(j,0,n-1){
+		REP(j,1,n){
 			if(i>=fa[j]){
 				dpa[i]=max(dpa[i],dpa[i-fa[j]]+c[j]);
 			}
@@ -83,7 +83,7 @@ void Solve(){
 	}
 
 	REP(i,minb,f){
-		REP(j,0,n-1){
+		REP(j,1,n){
 			if(j>=fb[j]){
 				dpb[i]=max(dpb[i],dpb[i-fb[j]]+c[j]);
 			}
