439cd831955807e790a88b32cbd79e4400db9d78
czfshine <czfshine@outlook.com>
2017-05-11 10:17:16 +0800

    build erp4c

--- project/ERP4C/testgoods.c
+++ project/ERP4C/testgoods.c
@@ -5,13 +5,13 @@ int main(){
 	Store * S;
 	S=InitStore();
 
-	AddGoods(S,1,"1111",2);
-	AddGoods(S,2,"2111",3);
+	AddGoods(*S,1,"1111",2);
+	AddGoods(*S,2,"2111",3);
 
-	ChangeGoodsName(S,1,"5555");
+	ChangeGoodsName(*S,1,"5555");
 
-	ChangeGoodsCount(S,2,555);
+	ChangeGoodsCount(*S,2,555);
 
-	
+
 	return 0;
 }
