8cf4a2d25c066084de963aa637b6844b285b07ba
czfshine <czfshine@outlook.com>
2017-05-11 19:48:02 +0800

    build erp4c

--- project/ERP4C/main.c
+++ project/ERP4C/main.c
@@ -87,7 +87,7 @@ int LogginGoods(){
 	
 	return 0;
 }
-/*
+
 int ChangeName(goods* g){
 	char * name;
 	name=WantName();
@@ -96,7 +96,7 @@ int ChangeName(goods* g){
 	g->name=name;
 	return 0;
 }
-*/
+
 int ChangeCount(goods * g){
 
 	int count ;
