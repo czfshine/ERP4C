4f47bfd1fe0ddc304822bcc88a26fcf34e51cce8
czfshine <czfshine@outlook.com>
2017-05-11 10:16:26 +0800

    build erp4c

--- project/ERP4C/testgoods.c
+++ project/ERP4C/testgoods.c
@@ -1,5 +1,17 @@
 #include "goods.h"
 
 int main(){
+
+	Store * S;
+	S=InitStore();
+
+	AddGoods(S,1,"1111",2);
+	AddGoods(S,2,"2111",3);
+
+	ChangeGoodsName(S,1,"5555");
+
+	ChangeGoodsCount(S,2,555);
+
+	
 	return 0;
 }
