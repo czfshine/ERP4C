ab65b41ca1d3dba317f72924f7bf3341ec68e7e6
czfshine <czfshine@outlook.com>
2017-05-13 01:29:44 +0800

    build erp4c

--- common.h
+++ common.h
@@ -4,6 +4,24 @@
 #define MAKE(type) (type * ) malloc(sizeof(type));
 
 #define LOOP(fn) while(!fn()){};
+int DEBUG = 0;
+#ifdef LOCAL
+DEBUG = 1;
+#endif 
+
+#define WithDebug(code) do{\
+	if(DEBUG){\
+		int oldstdout = dup(1);\
+		freopen("testin1.txt", "r", stdin);\
+		freopen("testout1.txt", "w", stdout);\
+		code;\
+		freopen("CON", "r", stdin);\
+		freopen("CON", "w", stdout);\
+		dup2(oldstdout, 1);} \
+	else {\
+			code;\
+		}\
+}while (0);
 
 
 
