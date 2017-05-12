05f18e9fa0fe194c6eb976ad6b87c7f3956be98f
czfshine <czfshine@outlook.com>
2017-05-13 01:34:51 +0800

    build erp4c

---
+++ project/ERP4C/debug.h
@@ -0,0 +1,21 @@
+
+#ifndef LOCAL
+
+int DEBUG = 0;
+#else
+int DEBUG = 1;
+#endif 
+
+#define WithDebug(code) do{\
+	if(DEBUG){\
+		int oldstdout = dup(1);\
+		freopen("testin1.txt", "r", stdin);\
+		freopen("testout1.txt", "w", stdout);\
+		code;\
+		freopen("CON", "r", stdin);\
+		freopen("CON", "w", stdout);\
+		dup2(oldstdout, 1);} \
+	else {\
+			code;\
+		}\
+}while (0);
