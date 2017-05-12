092e3c65ab884e0a3beb29ee97ab34e8b0eaacea
czfshine <czfshine@outlook.com>
2017-05-13 01:34:51 +0800

    build erp4c

--- project/ERP4C/common.h
+++ project/ERP4C/common.h
@@ -5,28 +5,6 @@
 
 #define LOOP(fn) while(!fn()){};
 
-#ifndef LOCAL&&D
-#define D
-int DEBUG = 0;
-#else
-#define D
-int DEBUG = 1;
-#endif 
-
-#define WithDebug(code) do{\
-	if(DEBUG){\
-		int oldstdout = dup(1);\
-		freopen("testin1.txt", "r", stdin);\
-		freopen("testout1.txt", "w", stdout);\
-		code;\
-		freopen("CON", "r", stdin);\
-		freopen("CON", "w", stdout);\
-		dup2(oldstdout, 1);} \
-	else {\
-			code;\
-		}\
-}while (0);
-
 
 
 #endif
