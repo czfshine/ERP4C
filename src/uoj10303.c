7b5fc5c9899813f909e4e8eda0812679cf9314dc
czfshine <czfshine@outlook.com>
2017-05-16 23:36:14 +0800

    updata

--- src/uoj10303.c
+++ src/uoj10303.c
@@ -48,8 +48,15 @@ int n,d[100][100],a[100][100],j=0;
 
 
 
-int solve(int i, int j){    
-	if(d[i][j] >= 0) return d[i][j];    
+int init(){
+    int i,k;
+
+    for ( i = 0; i <n; ++i)
+		for (k = 0; k <= i; ++k)
+			scanf("%d",&a[i][k]);
+}
+int solve(int i, int j){
+	if(d[i][j] >= 0) return d[i][j];
 	return d[i][j] = a[i][j] +(i == n ? 0 : max(solve(i+1,j),solve(i+1,j+1)));
 }
 
@@ -57,17 +64,10 @@ int solve(int i, int j){
 int main(int argc, char const *argv[])
 {
 	scanf("%d",&n);
-	for (int i = 0; i <n; ++i)
-		for (int k = 0; k <= i; ++k)
-			scanf("%d",&a[i][k]);
-
+	init();
 	memset(d,-1,sizeof(d));
 	printf("%d\n",solve(0,0));
 
-	for (int i = 0; i <n; ++i){	
-		printf("%d ",a[i][d[i][j]>d[i][j+1]?j:j++]);
 
-	
 	return 0;
-}
 }
