4c225820bf51bd0d3bcf6683b63069a2afb12b15
czfshine <czfshine@outlook.com>
2017-05-17 08:42:48 +0800

    build erp4c

---
+++ project/ERP4C/serialize.h
@@ -0,0 +1,11 @@
+#include "goods.h"
+#include "linklist.h"
+
+
+
+#define writerfn(name) int(* name)(FILE* ,void *)
+#define readfn(name)	void*(*name)(FILE *)
+
+int serialize(Store s, FILE * fp, writerfn(writer));
+
+int unserialize(Store s, FILE * fp, readfn(reader));
