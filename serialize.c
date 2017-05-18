7ccc92cfdf584b71f1f3c79632f813d7a02ba986
czfshine <czfshine@outlook.com>
2017-05-18 18:35:17 +0800

    build erp4c

--- serialize.c
+++ serialize.c
@@ -1,28 +1,31 @@
 #include "serialize.h"
 
-int pretretment(LinkList L,ptr p,writerfn(writer)){
+int pretretment(LinkList L, ptr p, writerfn(writer)) {
 	StoreInfo *si;
-	si= L->elem;
+	si = L->elem;
 	goods *g;
-	g=(goods *) p;
+	g = (goods *)p;
 
-	if(writer(si->file,g)){
+	if (writer(si->file, g)) {
 		return 1;
-	}else{
+	}
+	else {
 		return 0;
 	}
 }
 
 
-int serialize(Store s,FILE * fp,writerfn(writer)){
-	SI(s)->file=fp;
-	foreachwithcmp(s.L,pretretment,writer);
+int serialize(Store s, FILE * fp, writerfn(writer)) {
+	SI(s)->file = fp;
+	foreachwithcmp(s.L, pretretment, writer);
+	return 0;
 }
 
-int unserialize(Store s , FILE * fp,readfn(reader)){
+int unserialize(Store s, FILE * fp, readfn(reader)) {
 
 	void * elem;
-	while(elem=reader(fp)){
-		pushfront(s.L,elem);
+	while (elem = reader(fp)) {
+		pushfront(s.L, elem);
 	}
+	return 0;
 }
