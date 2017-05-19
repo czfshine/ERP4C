9545cee1df204454943114b21fa4f0bfe82e7b3c
czfshine <czfshine@outlook.com>
2017-05-19 15:38:13 +0800

    build erp4c

--- outtestcase.lua
+++ outtestcase.lua
@@ -2,11 +2,11 @@ outfile=io.open("testin1.txt","w")
 
 outfile:write("1\n");
 
-for i=1,1000000 do
+for i=1,10000 do
 	outfile:write(tostring(i).."\n");
 	outfile:write("name"..tostring(i).."\n");
 	outfile:write(tostring(i).."\n");
 	outfile:write("\n");
 end
 
-outfile:write("0\n\n\n\n\n");
+outfile:write("0\n\n\n0\n\n");
