0ee5f9db61ac1367d00b762b28dd8b3d3755f921
czfshine <czfshine@outlook.com>
2017-05-19 10:07:54 +0800

    build erp4c

--- linklist.h
+++ linklist.h
@@ -28,14 +28,14 @@ typedef struct LinkNode
 	ptr elem;
 	struct LinkNode *next;
 }  LinkNode, *LinkList;
+#define cmpfn(cmp)  int(*cmp)(ptr, ptr)
 
-
-LinkList  LinkListInit();
-LinkNode * newnode(ptr elem);
-LinkNode * back(LinkList L);
-int pushback(LinkList L, ptr elem);
-int pushfront(LinkList L, ptr elem);
-LinkNode * foreachwithcmp(LinkList L, int(*fn)(LinkList, ptr, int(*cmp)(ptr, ptr)), int(*cmp) (ptr, ptr));
-LinkNode * foreach(LinkList L, int(*fn)(LinkList, ptr));
-int RemoveNode(LinkNode *p, int(*freeelem)(ptr));
+LinkList  LinkListInit();/*初始化*/
+LinkNode * newnode(ptr elem);/*新节点*/
+LinkNode * back(LinkList L);/*返回最后一个节点*/
+int pushback(LinkList L, ptr elem);/*加到最后*/
+int pushfront(LinkList L, ptr elem);/*加到最前*/
+LinkNode * foreach(LinkList L, int(*fn)(LinkList, ptr));/*遍历*/
+LinkNode * foreachwithcmp(LinkList L, int(*fn)(LinkList, ptr, cmpfn(cmp)), cmpfn(cmp));/*带比较函数的遍历*/
+int RemoveNode(LinkNode *p, int(*freeelem)(ptr));/*删除节点*/
 #endif
